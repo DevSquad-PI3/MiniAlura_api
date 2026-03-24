dd6af3d47d4cbbd7151063bc4926dea80940c0bc
Kleby <kleby10@gmail.com>
2026-03-24 11:09:36 -0300

    :tada: commit inicial

---
+++ src/main/java/com/devsquad/pi3/minialura/MinialuraApplication.java
@@ -0,0 +1,13 @@
+package com.devsquad.pi3.minialura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinialuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinialuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsquad/pi3/minialura/MinialuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsquad.pi3.minialura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinialuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
